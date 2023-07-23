bd4d32ea68a07f81184708565bad23885ca3069e
shraaavss <shravs2707@gmail.com>
2023-07-23 15:39:39 +0530

    minor changes

--- src/pages/components/Nav.jsx
+++ src/pages/components/Nav.jsx
@@ -29,54 +29,54 @@ export default function Nav() {
 
     return (
 
-            <nav className="nav-bar">
+        <nav className="nav-bar">
 
-                {/* Checks if we're on mobile or not */}
-                {matches && (
-                    <div className="nav-links flex gap-10 text-white">
-                        <a className='nav-link-text' href="/Gallery">Gallery</a>
-                        <a className='nav-link-text' href="/About">About Us</a>
-                        <a className='nav-link-text' href="/Contact">Contact</a>
-                    </div>
-                )}
+            {/* Checks if we're on mobile or not */}
+            {matches && (
+                <div className="nav-links flex gap-10 text-white">
+                    <a className='nav-link-text' href="/Gallery">Gallery</a>
+                    <a className='nav-link-text' href="/About">About Us</a>
+                    <a className='nav-link-text' href="/Contact">Contact</a>
+                </div>
+            )}
 
 
-                {/* TOGGLE ICON */}
-                {!matches && (
-                    <div onClick={() => setToggled(prevToggled => !prevToggled)}
-                        className="space-y-1.5 cursor-pointer z-50">
-                        <motion.span
-                            animate={{ rotateZ: toggled ? 45 : 0, y: toggled ? 8 : 0, width: toggled ? 32 : 24 }}
-                            className="block h-0.5 w-6 bg-white"> </motion.span>
+            {/* TOGGLE ICON */}
+            {!matches && (
+                <div onClick={() => setToggled(prevToggled => !prevToggled)}
+                    className="space-y-1.5 cursor-pointer z-50">
+                    <motion.span
+                        animate={{ rotateZ: toggled ? 45 : 0, y: toggled ? 8 : 0, width: toggled ? 32 : 24 }}
+                        className="block h-0.5 w-6 bg-white"> </motion.span>
 
-                        <motion.span
-                            animate={{ width: toggled ? 0 : 24 }}
-                            className="block h-0.5 w-6 bg-white"> </motion.span>
+                    <motion.span
+                        animate={{ width: toggled ? 0 : 24 }}
+                        className="block h-0.5 w-6 bg-white"> </motion.span>
 
-                        <motion.span
-                            animate={{ rotateZ: toggled ? -45 : 0, y: toggled ? -8 : 0, width: toggled ? 32 : 24 }}
-                            className="block h-0.5 w-6 bg-white"> </motion.span>
-                    </div>
-                )}
+                    <motion.span
+                        animate={{ rotateZ: toggled ? -45 : 0, y: toggled ? -8 : 0, width: toggled ? 32 : 24 }}
+                        className="block h-0.5 w-6 bg-white"> </motion.span>
+                </div>
+            )}
 
-                {toggled && !matches && (
+            {toggled && !matches && (
+                <motion.div
+                    variants={navMotion}
+                    initial="hidden"
+                    animate="visible"
+                    className="fixed flex bg-neutral-500 bottom-0 left-0 w-full h-screen justify-center items-center">
+
                     <motion.div
-                        variants={navMotion}
-                        initial="hidden"
-                        animate="visible"
-                        className="fixed flex bg-neutral-500 bottom-0 left-0 w-full h-screen justify-center items-center">
-
-                        <motion.div
-                            className="nav-links-toggle flex flex-col gap-20 text-lg items-center justify-center">
-                            <motion.a variants={itemMotion} href="./About.astro">About Us</motion.a>
-                            <motion.a variants={itemMotion} href="/">Gallery</motion.a>
-                            <motion.a variants={itemMotion} href="/Contact">Contact</motion.a>
-                        </motion.div>
+                        className="nav-links-toggle flex flex-col gap-20 text-lg items-center justify-center">
+                        <motion.a variants={itemMotion} href="./About.astro">About Us</motion.a>
+                        <motion.a variants={itemMotion} href="/">Gallery</motion.a>
+                        <motion.a variants={itemMotion} href="/Contact">Contact</motion.a>
                     </motion.div>
+                </motion.div>
 
-                )}
+            )}
 
-            </nav>
+        </nav>
 
     )
 }
